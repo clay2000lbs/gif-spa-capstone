05c5ae8ad773b2ae5282af525b98905133bde89a
Clayton J. Wilkerson <clay2000lbs@gmail.com>
2022-01-31 19:52:41 -0600

    trying to add logo

--- gif-spa/src/component/Nav.js
+++ gif-spa/src/component/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 function Nav() {
   return (
   <div className="nav">
     <h1>Gif_Spa</h1>
+    {/* <img src={spa} alt="Spa Icon" /> */}
     <div className="sub-nav">
         <h3>Sign Up</h3>
         <h3>Sign In</h3>
